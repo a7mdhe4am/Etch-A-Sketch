af41927fc2e63323626895f8bd31a596cf9cbd53
a7mdhe4am <90359244+a7mdhe4am@users.noreply.github.com>
2022-07-10 18:24:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function createGrid(number){
+    const gridContainer= document.querySelector(".grid-container");
+    gridContainer.style.gridTemplateColumns= `repeat(${number}, 1fr)`;
+    for (let i=1; i<= number*number; i++){
+        let div= document.createElement("div");
+        div.classList.add("grid-box");
+        gridContainer.appendChild(div);
+    }
+    document.querySelector(".grid-dimensions").textContent= `${number}x${number}`;
+}
+
+function changecolor(event){
+    event.target.style.backgroundColor= "black";
+
+}
+
+function resetGrid(){
+    let num = prompt("please enter grid size less than or equal to 100:");
+    if (num === undefined || num === null || num === ""){
+        return;
+    }
+    while (num >100){
+        num = prompt("Number too big! please enter grid size less than or equal to 100:");
+    }
+    const gridContainer = document.querySelector(".grid-container");
+    while (gridContainer.hasChildNodes()){
+        gridContainer.removeChild( gridContainer.firstChild);
+    }
+    createGrid(num);
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changecolor)});
+}
+
+function clearGrid(){
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.style.backgroundColor = null});
+}
+
+let gridNumber = 16;
+createGrid(gridNumber);
+let gridBoxList = document.querySelectorAll(".grid-box");
+gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changecolor)});
